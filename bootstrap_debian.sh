9c32144a6689bf1cc3db7a2a6a35e390c35ef24f
Niklaus Giger <niklaus.giger@member.fsf.org>
2013-03-07 14:54:09 +0100

    libvirt-dev needed for vagrant

--- bootstrap_debian.sh
+++ bootstrap_debian.sh
@@ -50,7 +50,7 @@ echo "(Re)installing needed packets for RVM and ruby"
 apt-get --no-install-recommends install bash curl git patch bzip2 \
   build-essential openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev \
   libssl-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf \
-  libc6-dev libgdbm-dev ncurses-dev automake libtool bison subversion libffi-dev
+  libc6-dev libgdbm-dev ncurses-dev automake libtool bison subversion libffi-dev libvirt-dev
 
 rvm install 1.9.2
 gem install --no-ri --no-rdoc puppet
