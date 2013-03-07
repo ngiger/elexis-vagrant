4c0d33793dbb62e9f4786d82009cdd0740aa6519
Niklaus Giger <niklaus.giger@member.fsf.org>
2013-03-07 09:24:21 +0100

    Create correct hiera.yaml while bootstrapping

--- bootstrap_debian.sh
+++ bootstrap_debian.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 # Copyright 2013 by Niklaus Giger niklaus.giger@member.fsf.org
-
-if [[ -d /etc/puppet/.git ]]
-then
-  echo "/etc/puppet has already a .git directory. Therefore we don't reinstall it"
-  exit
-else
-  echo "Installing into /etc/puppet"
-fi
+# Just download it using wget/curl https://raw.github.com/ngiger/elexis-vagrant/with_hiera/bootstrap_debian.sh
+# and execute it afterwards. E.g sudo -i $PWD/bootstrap_debian.sh
 
 debInst()
 {
@@ -17,13 +11,31 @@ debInst()
 packets_needed="curl git virtualbox virtualbox-dkms"
 for j in $packets_needed
 do
-  echo "j ist $j"
   if debInst $j; then
       echo $j seems to be already installed
   else
       apt-get install $j
   fi
 done
+
+origin='git://github.com/ngiger/elexis-vagrant.git'
+dest='/etc/puppet'
+if [[ -d ${dest}/.git ]]
+then
+  echo "${dest} has already a .git directory. Therefore we don't reinstall it"
+else
+  echo "Installing into ${dest}"
+  if ! [[ -d /etc/puppet/ ]]
+  then
+    git clone ${origin} ${dest}
+  else
+    # move it out of the way
+    tempDir=`mktemp -d`
+    mv ${dest} ${tempDir}
+    git clone ${origin} ${dest}
+    mv --no-clobber ${tempDir}/* ${dest}
+  fi
+fi
 
 RVM_MULTI_PATH='/usr/local/rvm/bin/rvm'
 if [[ -e $RVM_MULTI_PATH ]]
@@ -40,6 +52,40 @@ apt-get --no-install-recommends install bash curl git patch bzip2 \
   libssl-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf \
   libc6-dev libgdbm-dev ncurses-dev automake libtool bison subversion libffi-dev
 
-. ~/.bash_profile # activate rvm  
 rvm install 1.9.2
 gem install --no-ri --no-rdoc puppet
+
+HIERA_PUPPET_YAML='/etc/puppet/hiera.yaml'
+if [[ -f ${HIERA_PUPPET_YAML} ]]
+then
+  echo "${HIERA_PUPPET_YAML} already present"
+else
+  echo "Adding default configuration into ${HIERA_PUPPET_YAML}"
+(
+cat <<'EOF'
+---
+:backends: yaml
+:yaml:
+  :datadir: ${dest}
+:hierarchy:
+  - %{::clientcert}
+  - %{::environment}
+  - private_hiera/config
+  - hiera/common
+:logger: console
+EOF
+) > $HIERA_PUPPET_YAML
+fi
+
+HIERA_YAML='/etc/hiera.yaml'
+if [[ ${HIERA_YAML} ]]
+then
+  echo "${HIERA_YAML} already present"
+else
+  echo "Creating logical link from ${HIERA_PUPPET_YAML} -> ${HIERA_YAML}"
+  ln -s ${HIERA_PUPPET_YAML} ${HIERA_YAML}
+fi
+ln -s ${HIERA_PUPPET_YAML} ${HIERA_YAML}
+ls -l ${HIERA_PUPPET_YAML} ${HIERA_YAML}
+
+echo "Cloned ${origin} into ${dest} and created default hiera configuration in ${HIERA_PUPPET_YAML}"
