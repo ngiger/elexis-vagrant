6e9bd9ec38ddb8d5a7f22f57c16f945758fb5db2
Niklaus Giger <niklaus.giger@member.fsf.org>
2013-04-20 00:33:38 +0200

    Mysql dump/load. Logrotate for dumps

--- static-modules/elexis/manifests/mysql_server.pp
+++ static-modules/elexis/manifests/mysql_server.pp
@@ -2,19 +2,27 @@
 # mysql-server environment for Elexis
 
 class elexis::mysql_server(
-  $db_backup_dir      = hiera('::db::backup::dir', '/home/backup'),
-  $db_backup_user     = hiera('::db::backup::user', 'elexis'),
-  $db_backup_password = hiera('::db::backup::name', 'elexisTest'),
-  $db_main_name       = hiera('::db::main::password', 'elexis'),
-  $db_main_user       = hiera('::db::main::user', 'elexis'),
-  $db_main_password   = hiera('::db::main::password', 'elexisTest'),
-  $db_test_name       = hiera('::db::test::name', 'tst_db'),
-  $db_my_dump_script  = hiera('::db::backup::dump_script', '/usr/local/bin/my_dump_elexis.rb')
+  $mysql_backup_dir        = hiera('elexis::mysql_server::mysql_backup_dir',       '/home/backups/mysql'),
+  $mysql_dump_dir          = hiera('elexis::mysql_server::mysql_dump_dir',         '/home/backup-mysql'),
+  $mysql_main_db_name      = hiera('elexis::mysql_server::mysql_main_db_name',     'elexis'),
+  $mysql_main_db_user      = hiera('elexis::mysql_server::mysql_main_db_user',     'elexis'),
+  # puppet apply --execute 'notify { "test": message => mysql_password("elexis", "elexisTest") }' --modulepath /vagrant/modules/
+  $mysql_main_db_password  = hiera('elexis::mysql_server::mysql_main_db_password', 'elexisTest'),
+  $mysql_tst_db_name       = hiera('elexis::mysql_server::mysql_tst_db_name',      'tst_db'),
+
 ){
 
 }
 
 class elexis::mysql_server inherits elexis::common {
+  class { 'mysql::server': 
+    config_hash => { 
+      'root_password' => hiera('mysql::server:root_password', 'elexisTest'),
+      # [mysqld]
+      
+    } ,
+  }
+
   # With this file we ensure that the mysql root password is elexisTest!
   file { '/etc/mysql/debian.cnf':
     ensure => present,
@@ -23,49 +31,34 @@ class elexis::mysql_server inherits elexis::common {
     source => 'puppet:///modules/elexis/debian.cnf',
   }
 
-  # notify { "Granting for Elexis MySQL database": }
-  package{  ['cups', 'cups-bsd']:
-    ensure => present,
-  }
-  
-  class { 'mysql::server':
-  # package_name  => 'x', # hiera('mysql::server:package_name', 'mysql-server-5.5'),
-  config_hash => { 'root_password' => hiera('mysql::server:root_password', 'elexisTest') } ,
-  }
-
-  mysql::db { "$db_main_name":
-    user     => $db_main_user,
-    password => $db_main_password,
+  notify{"m $mysql_main_db_name t $mysql_tst_db_name": }
+  if (0 == 1) {
+  mysql::db { "$mysql_main_db_name":
+    user     => $mysql_main_db_user,
+    password => $mysql_main_db_password,
     host     => 'localhost',
     ensure => present,
     charset => 'utf8',      
   }
-
-  if $db_test_name {
-    mysql::db { "$db_test_name":
-      user     => $db_main_user,
-      password => $db_main_password,
-      host     => 'localhost',
-      ensure => present,
-      charset => 'utf8',      
+}
+  if $mysql_tst_db_name {
+    database { "$mysql_tst_db_name":
+      ensure  => 'present',
+      charset => 'utf8',
     }
   }
-  
-  database_user{ 'vagrant@localhost':
-    ensure        => present,
-    password_hash => mysql_password(hiera('db::user:vagrant:password', 'vagrant')),
-    require       => Class['mysql::server'],
-  }
 
+  file {'/etc/mysql/conf.d/lowercase.conf':
+    ensure => present,
+    content => "[mysqld]\nlower_case_table_names = 1\n",
+    owner => root,
+    group => root,
+    mode => 0644,
+  }
+  
   database_user{ 'niklaus@localhost':
     ensure        => present,
     password_hash => mysql_password(hiera('db::user:giger:password', 'giger')),
-    require       => Class['mysql::server'],
-  }
-
-  database_user{ 'arzt@localhost':
-    ensure        => present,
-    password_hash => mysql_password(hiera('db::user:arzt:password', 'aeskulap')),
     require       => Class['mysql::server'],
   }
 
@@ -74,22 +67,77 @@ class elexis::mysql_server inherits elexis::common {
     privileges => ['all'] ,
   }
     
-  exec { "create_mysql_dump_dir_path":
-    command => "mkdir -p `dirname ${db_backup_dir}`",
-    path => '/usr/bin:/bin',
-    unless => "[ -d `dirname ${db_backup_dir}` ]"
+  $db_mysql_dump_script = "/usr/local/bin/mysql_dump_elexis.rb"
+  file {$db_mysql_dump_script:
+    ensure => present,
+    mode   => 0755,
+    content => template("elexis/mysql_dump_elexis.rb.erb"),
+#    require => File['/usr/local/bin/pg_util.rb'],
   }
 
-  $backupdir      = $db_backup_dir
-  $backupuser     = $db_backup_user
-  $backuppassword = $db_backup_password
-  
-  # see http://puppetlabs.com/blog/module-of-the-week-puppetlabs-mysql-mysql-management/
-  # define test-deb (anonymized) # TODO: Vierte Priorität (Nice to have)
-  class { 'mysql::backup':
-    backupuser     => $db_backup_user,
-    backuppassword => $backuppassword,
-    backupdir      => $db_backup_dir,
+  $db_mysql_load_script = "/usr/local/bin/mysql_load_tst_db.rb"
+  file {$db_mysql_load_script:
+    ensure => present,
+    mode   => 0755,
+    content => template("elexis/mysql_load_tst_db.rb.erb"),
+#    require => File['/usr/local/bin/pg_util.rb'],
+  }
+
+  exec { "$mysql_backup_dir":
+    command => "mkdir -p $mysql_backup_dir",
+    path => '/usr/bin:/bin',
+    creates => "$mysql_backup_dir"
+  }
+
+  file { $mysql_backup_dir :
+    ensure => directory,
+    mode   => 0755,
+    recurse => true,
+    owner  =>  $::mysql::params::user, group => $::mysql::params::group,
+    require     => Exec["$mysql_backup_dir"],
+  }
+    
+  exec { "$mysql_dump_dir":
+    command => "mkdir -p ${mysql_dump_dir}",
+    path => '/usr/bin:/bin',
+    creates => "$mysql_dump_dir"
+  }
+
+  file { $mysql_dump_dir :
+    ensure => directory,
+    mode   => 0755,
+    recurse => true,
+    owner  =>  $::mysql::params::user, group => $::mysql::params::group,
+    require     => Exec["$mysql_dump_dir"],
+  }
+    
+  cron { 'mysql-elexis-backup':
+      ensure  => $ensure,
+      command => "$db_mysql_dump_script",
+      user    => 'root',
+      hour    => 23,
+      minute  => 15,
+      require => [
+        File["$db_mysql_dump_script", "$mysql_backup_dir"],
+        Exec["$mysql_backup_dir", "$mysql_dump_dir"],
+      ]
+      
+  }
+
+  file {'/etc/logrotate.d/mysql_elexis_dump':
+    ensure => present,
+    content => "\n${$mysql_dump_dir}/elexis.dump.gz {
+    rotate 10
+    daily
+    missingok
+    notifempty
+    create 0640 root root
+    nocompress
+}
+",
+    owner => root,
+    group => root,
+    mode  => 0644,
   }
 
 }
