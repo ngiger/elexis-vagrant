e27d50d794097685b9173c2bb31ef323ad9b5e6f
Niklaus Giger <niklaus.giger@member.fsf.org>
2013-04-23 21:43:57 +0200

    Unattendend upgrades, postgres added

--- static-modules/elexis/manifests/devel.pp
+++ static-modules/elexis/manifests/devel.pp
@@ -2,12 +2,15 @@
 
 class elexis::devel inherits elexis::common {
 
+  include elexis::admin
   include elexis::client
   include elexis::mysql_server
+  include elexis::postgresql_server
   include eclipse
   include jenkins
   include elexis::jenkins_commons
-  elexis::jenkins_elexis{'2.1.6': branch => '2.1.6'} # Adds Jenkins and jubula tests for Elexis 2.1.6
+#  elexis::jenkins_elexis{'2.1.6': branch => '2.1.6'} # Adds Jenkins and jubula tests for Elexis 2.1.6
+  elexis::jenkins_elexis{'2.1.7': branch => '2.1.7'} # Adds Jenkins and jubula tests for Elexis 2.1.6
 
 #  include elexis::jenkins_2_2_dev_jpa # TODO: Zweite Priorität , dito 2.1.6/zdavatz and buildr)
 #  include buildr # TODO: Dritte Priorität, da mit 2.1.7 mit ant gebuildet werden kann
