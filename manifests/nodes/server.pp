2027c1c932d42795de8aea3333eca6d865b1af2e
Niklaus Giger <niklaus.giger@member.fsf.org>
2013-11-23 21:41:33 +0100

    Don't install praxis_wiki for the moment

--- manifests/nodes/server.pp
+++ manifests/nodes/server.pp
@@ -3,7 +3,7 @@ node "server" {
   # notify{"Adding node server": }
   file{'/etc/system_role': content => "server\n" }
   # These are the defaults for the node called server
-  if hiera('elexis::praxis_wiki::ensure', true)        { include elexis::praxis_wiki }
+#  if hiera('elexis::praxis_wiki::ensure', true)        { include elexis::praxis_wiki }
   if hiera('elexis::postgresql_server::ensure', true)  { include elexis::postgresql_server }
   if hiera('elexis::mysql_server::ensure', true)       { include elexis::mysql_server }
   if hiera('elexis::cockpit::ensure', true)            { include cockpit::service}
